84acdb9ffdb362d4f6f7fe0130fb491809779179
Phan Hoang <hoangphantich@gmail.com>
2017-02-17 21:46:51 +0700

    Session_04 #draft code, try first

---
+++ hoangpt/Session04/src/com/hoangphan/session04/Greeting.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hoangphan.session04;
+
+/**
+ * @author hoangpt
+ */
+class Greeting {
+
+  /**
+   * contructor method
+   */
+  public Greeting() {
+  }
+
+  void sayHello() {
+     System.out.println("Hello, how are you.");
+  }
+  
+  void sayHello(String name){
+    System.out.println("Hello "+name+" ^^");
+  }
+  
+  //signature
+  //access_modifier type name_function(param)
+  public String sayGoodbye(String name){
+    return "Goodbye "+name+".";
+  }
+  
+  private void sayHello(int langISO){
+    //hello in English
+    System.out.println("Hello Luong ^^");
+    
+    //hello in French
+    System.out.println("Bonjour Luong ^^");
+    
+    //hello in VN
+    System.out.println("Xin chao Luong ^^");
+  }
+}
---
+++ hoangpt/Session04/src/com/hoangphan/session04/Main.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hoangphan.session04;
+
+/**
+ *
+ * @author hoangpt
+ */
+public class Main {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    
+    //build tower
+    System.out.println("*");
+    System.out.println("**");
+    System.out.println("***");
+    
+    
+    buildCarpet();
+    buildCarpet();
+    
+    //hello, new className
+    new Greeting().sayHello();
+  }
+  
+  /**
+   * behavior
+   */
+  static void buildCarpet(){
+    //build carpet
+    System.out.println("---");
+
+    System.out.println("***");
+    System.out.println("***");
+    System.out.println("***");
+  }
+}
